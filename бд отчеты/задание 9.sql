2c45b2d44559595bf55c4b3729e6f2dfbcec9b8e
az299best <167977256+az299best@users.noreply.github.com>
2026-09-17 22:24:04 +0300

    Add files via upload

---
+++ бд отчеты/задание 9.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM simpledb.resume
+LIMIT 0, 1000
+
+-- Date: 2026-08-05 11:07
+*/
+INSERT INTO `` (`resumeid`,`userid`,`title`,`skils`,`created`) VALUES (1,1,'студент','среднее общее образование','2024-08-07 00:00:00');
+INSERT INTO `` (`resumeid`,`userid`,`title`,`skils`,`created`) VALUES (2,2,'стоматолог','высшее медецинское образование','2005-04-03 00:00:00');
+INSERT INTO `` (`resumeid`,`userid`,`title`,`skils`,`created`) VALUES (3,3,'школьник','начальное образование','2022-06-07 00:00:00');
---
+++ бд отчеты/запросы.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM simpledb.users
+LIMIT 0, 1000
+
+-- Date: 2026-08-05 10:36
+*/
+INSERT INTO `` (`id`,`name`,`email`,`gender`,`bday`,`postal_code`,`rating`,`created`) VALUES (1,'Nickolas','kiselev9905@gmail.com',NULL,NULL,NULL,NULL,'2026-08-02 14:07:39');
+INSERT INTO `` (`id`,`name`,`email`,`gender`,`bday`,`postal_code`,`rating`,`created`) VALUES (2,'Natalya','vagner7.76@mail.ru',NULL,NULL,NULL,NULL,'2026-08-02 14:07:39');
+INSERT INTO `` (`id`,`name`,`email`,`gender`,`bday`,`postal_code`,`rating`,`created`) VALUES (3,'Maxim','Max471276@mail.ru',NULL,NULL,NULL,NULL,'2026-08-02 14:07:39');
+INSERT INTO `` (`id`,`name`,`email`,`gender`,`bday`,`postal_code`,`rating`,`created`) VALUES (4,'Ekaterina','ekaterina.petrova@outlook.com','F','2000-02-11','145789',1.123,'2026-08-05 10:29:04');
+INSERT INTO `` (`id`,`name`,`email`,`gender`,`bday`,`postal_code`,`rating`,`created`) VALUES (5,'Paul','paul@superpochta.ru','M','1998-08-12','123789',1,'2026-08-05 10:29:04');
